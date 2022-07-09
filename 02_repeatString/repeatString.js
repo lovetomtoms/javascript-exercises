ddafd5916f167a6bf859e0f980c662b80ceea519
lovetomtoms <laraglov6@gmail.com>
2022-07-09 21:52:31 +0800

    Put codes to function reverse string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str ,num) {
+    let repeat = "";
+    if (num < 0){
+        return "ERROR";   
+    }else{
+        for (let i = 0; i < num; i++) {
+            repeat += str;
+        }
+        return repeat;
+    }    
 };
 
 // Do not edit below this line
