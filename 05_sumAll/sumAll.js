3c20dbf99d35bc1ff0104d20d436f0a11481a1d1
lovetomtoms <laraglov6@gmail.com>
2022-07-11 19:47:08 +0800

    Put codes to function to satisfy TTD

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let total = 0;
+    if ((typeof num1 === 'number' && typeof num2 === 'number') && (num1 > 0 && num2 >0)) {
+        if (num1 < num2) {
+            for (let i = num1; i <= num2; i++) {
+                total += i;
+            }
+            return total;
+        } else {
+            for (let i = num2; i <= num1; i++) {
+                total += i;
+            }
+            return total;
+        }
+    }else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
