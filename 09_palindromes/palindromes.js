fd81aeab7ab5871915d9aff9de70096da4ce3fd8
lovetomtoms <laraglov6@gmail.com>
2022-07-26 21:47:26 +0800

    Add codes to function in palindromes.js to satisfy test in palindromes.spec.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const remove = /\W/g;
+    const lowStr = str.replace(remove, '').toLowerCase();
+    const revStr = lowStr.split('').reverse().join('');
+    if(revStr === lowStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
