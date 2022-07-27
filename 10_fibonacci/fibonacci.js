6a2c942df8c0243f6842730e8593da550a9743f0
lovetomtoms <laraglov6@gmail.com>
2022-07-27 21:29:51 +0800

    Add codes to function in fibonacci.js to satisfy fibonacci.spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(Number(num) === NaN || num < 0) {
+        return 'OOPS';
+    }
+    if(typeof num === 'string') {
+        num = parseInt(num);
+    }
+    if(num === 1) {
+        return 1;
+    }
+    let fib = [0, 1];
+    for(let i = 2; i <= num; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+        if(num === i) {
+            return fib[i];
+        }
+    }
 };
 
 // Do not edit below this line
