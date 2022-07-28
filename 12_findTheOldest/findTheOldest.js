63f69369c8d7b6f178fb5ca0e3e57b5a0c56c2ca
lovetomtoms <laraglov6@gmail.com>
2022-07-28 22:46:47 +0800

    Add codes to function in findTheOldest.js to satisfy findTheOldest.spec.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let yearslived1 = 0;
+    const theOldest = people.reduce((oldest, person) => {
+        if(person.yearOfDeath == null) {
+            person['yearOfDeath'] = new Date().getFullYear();
+        }
+        const yearsLived2 = person['yearOfDeath'] - person.yearOfBirth;
+        if (yearsLived2 > yearslived1) {
+            yearslived1 = yearsLived2;
+            oldest = person;
+        }
+        return oldest;
+    }, {});
+    return theOldest;
 };
 
 // Do not edit below this line
